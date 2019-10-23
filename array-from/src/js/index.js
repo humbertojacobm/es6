13b59537d0ccd2733f551083c2e5fb19d1d69afe
humbertojacobm <humbertojacobm@gmail.com>
2019-10-22 23:47:44 -0500

    array-from how to use it

--- array-from/src/js/index.js
+++ array-from/src/js/index.js
@@ -1,3 +1,11 @@
 console.log("hi, array-from");
-const products = document.querySelectorAll('.product-price');
+
+const products = Array.from(document.querySelectorAll('.product-price'));
+
+products
+   .filter( product => parseFloat(product.innerHTML) <10)
+   .forEach( x => x.style.color = 'red');
+
+console.log('testing');
+
 console.log(products);
